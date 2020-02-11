06385e6a64c7e6f3e500e9c2a188cb925c52a66c
ekaone <ekaone3033@gmail.com>
2020-02-11 18:20:03 +0700

    👀

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { 
+  Landing,
+  SignIn,
+  SignUp,
+  ForgotPassword,
+  Dashboard 
+} from './views'
 
 function App() {
   return (
     <>
-      App
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/forgotpassword" component={ForgotPassword} />
+        </Switch>
+      </Router>
     </>
   );
 }
---
+++ src/views/index.js
@@ -0,0 +1,5 @@
+export { default as Landing } from './Landing'
+export { default as SignIn } from './SignIn'
+export { default as SignUp } from './SignUp'
+export { default as ForgotPassword } from './ForgotPassword'
+export { default as Dashboard } from './Dashboard'
