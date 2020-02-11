7b68c03f321ea7e43f2e174d8a68b64759fa1bed
ekaone <ekaone3033@gmail.com>
2020-02-11 18:45:21 +0700

    👀

--- src/views/ForgotPassword/ForgotPassword.js
+++ src/views/ForgotPassword/ForgotPassword.js
@@ -1,9 +1,88 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="primary" align="center">
+      {'Copyright © '}
+      <Link to="/" style={{ textDecoration: 'none', color: '#3f51b5' }}>
+        PT Canggih Teknik Indonesia
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function ForgotPassword() {
+  const classes = useStyles();
+
   return (
-    <>
-      Forgot Password
-    </>
-  )
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOpenIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5" color="textSecondary">
+          Forgot Password
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Enter your Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Reset Password
+          </Button>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
